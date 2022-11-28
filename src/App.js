b9394a1de2eea5186019aca884db2bc6007bf49b
Jamestran1304 <tlngocbao123@gmail.com>
2022-11-28 19:17:10 +0700

    first-commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import "./App.css";
+
+import Geekup1 from "./Components/exercise/Geekup1";
+import TaskApi from "./Components/exercise/TaskApi";
+
+function App() {
+  return (
+    <div className="App">
+      <Geekup1 />
+      <TaskApi/>
+    </div>
+  );
+}
+
+export default App;
